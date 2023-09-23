b710b7c2515ee112643ac111f3fdcf4c803812c1
unknown <joseroniz18@gmail.com>
2023-09-22 21:24:47 -0700

    agrege las variales nuevas

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -3,7 +3,9 @@ import { Sequelize } from 'sequelize';
 const sequelize = new Sequelize('almacen', 'root', '1234', {
     host: process.env.DB_HOST || 'localhost' ,
     dialect: 'mysql',
-    database: process.env.DB_NAME || 'almacen'
+    database: process.env.DB_NAME || 'almacen',
+    password: process.env.DB_PASSWORD || '1234',
+    username: process.env.DB_USER || 'root'
   });
 
   export default sequelize;
